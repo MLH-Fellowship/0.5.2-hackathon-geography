13715f36b3a4ac9fc1e11407827dbb797679f2fa
yk396 <yk396@cornell.edu>
2020-06-03 09:46:25 -0400

    Address PR changes

--- cli/main.py
+++ cli/main.py
@@ -1,16 +1,20 @@
 import sys
-import typer
 from functools import reduce
+
+import typer
 
 sys.path.append('../')
 from web.mappings import map 
 
+
 app = typer.Typer()
+
 
 @app.command()
 def get_word(char_code: str):
     word = map[char_code]
     typer.echo(f"{word}")
+
 
 @app.command()
 def get_phrase(code: str):
@@ -20,11 +24,11 @@ def get_phrase(code: str):
             typer.echo(error)
             return
         # Split string into array of 2 char elements.
-        char_arr = [code[i : i+2] for i in range(0, len(code), 2)]
+        char_arr = [code[i: i+2] for i in range(0, len(code), 2)]
         # Convert coded array to word phrase.
         phrase = reduce(lambda acc, x: acc + "." + map[x], char_arr, "")[1:]
         typer.echo(f"{phrase}")
-    except:
+    except KeyError:
         error = typer.style("Not a valid OLC.", fg=typer.colors.RED)
         typer.echo(error)
 
