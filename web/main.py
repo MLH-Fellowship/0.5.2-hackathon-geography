4eb34c2c66e571eaaa450eb17be0d2cd0cb1b9d8
Christopher Nguyen <chrisngyn99@gmail.com>
2020-06-03 13:57:47 -0400

    Initial commit

--- web/main.py
+++ web/main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/test')
+def index2():
+    return 'Dicking around with Flask'
+
+@app.route('/<name>')
+def index3(name):
+    return 'Hello {}'.format(name)
