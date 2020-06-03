bc3ec84a274d121eb1511a570f6ead6cb6476e07
Christopher Nguyen <chrisngyn99@gmail.com>
2020-06-03 15:20:54 -0400

    hotfixes

--- web/main.py
+++ web/main.py
@@ -5,16 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    # return render_template('index.html')
 
-# @app.route('/test')
-# def index2():
-#     return 'Dicking around with Flask'
 
-# @app.route('/<name>')
-# def index3(name):
-#     return 'Hello {}'.format(name)
+@app.route('/anything')
+def default():
+    return redirect(url_for('index'))
 
-# @app.route('/anything')
-# def index4():
-#     return redirect(url_for('index3', name='Kai'))
