a61eb59468fa59b4a3e4acd45ec13b079fbdaec6
Christopher Nguyen <chrisngyn99@gmail.com>
2020-06-03 14:59:17 -0400

    Skeleton webpage

--- web/main.py
+++ web/main.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+    # return render_template('index.html')
 
-@app.route('/test')
-def index2():
-    return 'Dicking around with Flask'
+# @app.route('/test')
+# def index2():
+#     return 'Dicking around with Flask'
 
-@app.route('/<name>')
-def index3(name):
-    return 'Hello {}'.format(name)
+# @app.route('/<name>')
+# def index3(name):
+#     return 'Hello {}'.format(name)
+
+# @app.route('/anything')
+# def index4():
+#     return redirect(url_for('index3', name='Kai'))
